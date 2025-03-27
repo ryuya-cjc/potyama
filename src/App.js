00b00b45761fb02e3fd0d8ccd9f19e71220ee7b3
ryuya-cjc <ryuya-yoshida@cresco-jcube.co.jp>
2025-03-27 12:44:17 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,24 @@
-import React, { useState } from 'react'; // useState をインポート
-import './App.css';
-import potymaImage from './potyma1.png'; // 画像をインポート
+import logo from "./logo.svg";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
 
-function App() {
-  // 入力フォームの値を管理するためのステートを追加
-  const [inputValue, setInputValue] = useState('');
-  const [displayText, setDisplayText] = useState('');
-
-  // 入力値が変わった時の処理
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-  };
-
-  // ボタンが押された時の処理
-  const handleButtonClick = () => {
-    alert("ボタンがクリックされました！");
-    setDisplayText(inputValue); // 入力したテキストを表示
-  };
-
+function App({ signOut }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* 画像を表示 */}
-        <img src={potymaImage} className="App-image" alt="Potyma" />
-
-        {/* 入力フォーム */}
-        <input
-          type="text"
-          value={inputValue}
-          onChange={handleInputChange}
-          placeholder="入力してください"
-          className="App-input"
-        />
-
-        {/* ボタン */}
-        <button onClick={handleButtonClick} className="App-button">
-          表示
-        </button>
-
-        {/* 入力された内容を表示 */}
-        {displayText && <p className="App-displayText">入力されたテキスト: {displayText}</p>}
-      </header>
-    </div>
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
